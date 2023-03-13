7b2f1dfd1fd38912038360654cce6c98f0de4120
Harshil Gupta <harshil.gupta@geminisolutions.com>
2023-03-13 19:35:33 +0530

    initial Commit

---
+++ src/main/java/com/example/awss3demoproject/AwsS3DemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.awss3demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3DemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3DemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awss3demoproject/AwsS3DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awss3demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3DemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
